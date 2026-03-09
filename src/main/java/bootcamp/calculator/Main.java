3c2256d504deb08d076bb17d96e77bba4489fbe9
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-03-09 12:35:14 +0100

    first commit.

---
+++ src/main/java/bootcamp/calculator/Main.java
@@ -0,0 +1,10 @@
+package bootcamp.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Que desea hacer?"+
+                                "\n1->Sumar dos numeros."+
+                                "\n2->Restar 2 numeros.\n"+
+                                "3->Multiplicar 2 numeros.");
+    }
+}
