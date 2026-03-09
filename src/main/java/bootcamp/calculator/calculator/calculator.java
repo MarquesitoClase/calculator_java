7553b0012161d94e5fd942721a05fabf3b550fc4
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-03-09 13:11:46 +0100

    feat: duplicated all methods and make it work with doubles changing the name too.

--- src/main/java/bootcamp/calculator/calculator/calculator.java
+++ src/main/java/bootcamp/calculator/calculator/calculator.java
@@ -12,4 +12,27 @@ public abstract class calculator {
     public static int integerProduct(int op1, int op2){
         return op1*op2;
     }
+
+    public static int integerDivision(int op1, int op2){
+        int resto = op1%op2;
+        if(resto!=0) System.out.println("Nos queda de resto: " + resto);
+        return op1/op2;
+    }
+
+
+    public  static double doubleSum(double op1, int op2){
+        return op1+op2;
+    }
+
+    public static double doubleSubtraction(int op1, int op2){
+        return op1-op2;
+    }
+
+    public static double doubleProduct(int op1, int op2){
+        return op1*op2;
+    }
+
+    public static double doubleDivision(double op1, double op2){
+        return op1/op2;
+    }
 }
