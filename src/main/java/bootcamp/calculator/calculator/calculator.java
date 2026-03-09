53f878f29e9495f0d288bd6353e2e82b7609f688
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-03-09 13:00:16 +0100

    feat: subtraction done.

--- src/main/java/bootcamp/calculator/calculator/calculator.java
+++ src/main/java/bootcamp/calculator/calculator/calculator.java
@@ -4,4 +4,7 @@ public abstract class calculator {
     public  static int integerSum(int op1, int op2){
         return op1+op2;
     }
+    public static int integerSubtraction(int op1, int op2){
+        return op1-op2;
+    }
 }
