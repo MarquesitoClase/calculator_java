57d5dfc7f72399a7f90dfec819f3fd9293273b9d
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-03-09 12:56:26 +0100

    first operation make on calculator(sum).

--- src/main/java/bootcamp/calculator/calculator/calculator.java
+++ src/main/java/bootcamp/calculator/calculator/calculator.java
@@ -1,4 +1,7 @@
 package bootcamp.calculator.calculator;
 
-public class calculator {
+public abstract class calculator {
+    public  static int integerSum(int op1, int op2){
+        return op1+op2;
+    }
 }
