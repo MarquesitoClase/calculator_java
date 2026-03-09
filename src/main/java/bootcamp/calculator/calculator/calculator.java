b334638a4ff56aefb4163c74ae2752f06dd567d7
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-03-09 13:03:29 +0100

    feat: make integerProduct.

--- src/main/java/bootcamp/calculator/calculator/calculator.java
+++ src/main/java/bootcamp/calculator/calculator/calculator.java
@@ -4,7 +4,12 @@ public abstract class calculator {
     public  static int integerSum(int op1, int op2){
         return op1+op2;
     }
+
     public static int integerSubtraction(int op1, int op2){
         return op1-op2;
+    }
+
+    public static int integerProduct(int op1, int op2){
+        return op1*op2;
     }
 }
